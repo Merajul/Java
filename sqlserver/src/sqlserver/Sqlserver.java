b0e50f73336a22516413bfae0ad0d0b5fdfa68b4
Md Merajul Islam <dawnmerajul@gmail.com>
2020-09-20 17:58:56 +0600

    mssql

---
+++ sqlserver/src/sqlserver/Sqlserver.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sqlserver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+/**
+ *
+ * @author NESSBIT
+ */
+public class Sqlserver {
+
+public static void main(String[] args) {
+
+        // Create a variable for the connection string.
+        String connectionUrl = "jdbc:sqlserver://27.147.156.155:1433;databaseName=sms;user=nessbit;password=TimeSpace5060#";
+
+        try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
+            
+            System.out.println("success");
+//            String SQL = "SELECT TOP 10 * FROM inbox";
+//            ResultSet rs = stmt.executeQuery(SQL);
+//
+//            // Iterate through the data in the result set and display it.
+//            while (rs.next()) {
+//                System.out.println(rs.getString("Sender") + " " + rs.getString("Receiver"));
+//            }
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+}
